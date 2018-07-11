88da558ddf189c25d25a8baea77552c2deb8af4d
U47317 <arunmohanmammen@yahoo.com>
2018-07-11 23:18:50 +0530

    Added Log 4j support

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -25,12 +26,15 @@ public class TestBase {
 	public static EventFiringWebDriver e_driver;
 	public WebEventListner webEventListner;
 	
+	Logger log = Logger.getLogger(TestBase.class);
+	
 	public TestBase()
 	{
 		try {
 			prop = new Properties();
 			FileInputStream ip = new FileInputStream("G:\\workspace\\FreeCRMTest\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
 			prop.load(ip);
+			log.info("Loaded Configuration file....");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,17 +47,21 @@ public class TestBase {
 		{
 			System.setProperty("webdriver.chrome.driver", "G:\\workspace\\chromedriver\\chromedriver.exe");
 			driver = new ChromeDriver();
+			log.info("Launching google chrome browser....");
+			
 		}
 		else if(browserName.equals("firefox"))
 		{
 			System.getProperty("webdriver.gecko.driver", "G:\\workspace\\firefoxdriver\\geckodriver.exe");
 			driver = new FirefoxDriver();
+			log.info("Launching Firefox browser...");
 		}
 		
 		e_driver = new EventFiringWebDriver(driver);
 		webEventListner = new WebEventListner();
 		e_driver.register(webEventListner);
 		driver=e_driver;
+		log.info("EventFiringWebDriver activated....");
 		
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -61,6 +69,7 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		
 		driver.get(prop.getProperty("url"));
+		log.info("URL launched successfully");
 		
 			 
 	}
